bec319a55052d8fe7037912a3ae43265932628f7
Shashank Shekhar <shekharshashank093@gmail.com>
2023-05-30 04:00:21 +0530

    new files were added

---
+++ hola.cpp
@@ -0,0 +1,23 @@
+//Shashank Shekhar
+//title: hola.cpp
+//date: 2023-05-30 03:59:01
+//Path: ~/Robotix/hola.cpp
+//bool cmp (pair<string,int> &a, pair <string ,int> &b){return a.second<b.second;}
+//int mx = *max_element(v.begin(), v.end())
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long int
+#define fi(n)        for(int i=0;i<n;i++)
+#define el endl;
+signed main() 
+{
+ios::sync_with_stdio(false);
+cin.tie(0);
+cout.tie(0);
+int t;
+cin>>t;
+while(t--)
+{
+    cout<<"Hellow world"<<el;
+}
+}
